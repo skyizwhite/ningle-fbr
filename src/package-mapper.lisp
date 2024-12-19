b0a9f643fc624ada37bbbefd9bc0e61c33814d80
paku <paku@skyizwhite.dev>
2024-12-20 00:39:41 +0900

    Implement uri-mapper and package-mapper

--- src/package-mapper.lisp
+++ src/package-mapper.lisp
@@ -1,4 +1,26 @@
 (defpackage #:ningle-fbr/package-mapper
   (:use #:cl)
+  (:import-from #:cl-ppcre
+                #:split
+                #:regex-replace)
   (:export #:path->package))
 (in-package #:ningle-fbr/package-mapper)
+
+(defun normalize-path (path)
+  (remove-if #'(lambda (str) (string= str ""))
+             (split "/" path)))
+
+(defun remove-system-path (routes-dir system-path)
+  (if system-path
+      (regex-replace (format nil "^~a/?" (cl-ppcre:quote-meta-chars system-path))
+                     routes-dir
+                     "")
+      routes-dir))
+
+(defun path->package (path &key system (system-path nil) routes-dir)
+  "Convert PATH to a package name based on given SYSTEM, SYSTEM-PATH, and ROUTES-DIR."
+  (let* ((cleaned-routes-dir (remove-system-path routes-dir system-path))
+         (routes-parts (normalize-path cleaned-routes-dir))
+         (path-parts (normalize-path path))
+         (all-parts (append (list system) routes-parts path-parts)))
+    (string-upcase (format nil "~{~a~^/~}" all-parts))))
--- src/uri-mapper.lisp
+++ src/uri-mapper.lisp
@@ -1,4 +1,16 @@
 (defpackage #:ningle-fbr/uri-mapper
   (:use #:cl)
+  (:import-from #:cl-ppcre)
   (:export #:path->uri))
 (in-package #:ningle-fbr/uri-mapper)
+
+(defun remove-index (str)
+  (if (string= str "/index")
+      "/"
+      (cl-ppcre:regex-replace "/index$" str "")))
+
+(defun colon->bracket (str)
+  (cl-ppcre:regex-replace-all "\\[(.*?)\\]" str ":\\1"))
+
+(defun path->uri (path)
+  (colon->bracket (remove-index path)))
